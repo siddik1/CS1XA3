4bce0dd087d503dcd7ef4bd14823a9f1e3ee1cae
Khizar Mohammad Siddiqui <siddik1@mills.cas.mcmaster.ca>
2018-02-26 22:51:27 -0500

    Updated

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+function check_status() {
 val= (git status | grep "Your branch is up-to-date")
 
 if [$val == "Your branch is up-to-date"]
@@ -8,13 +9,18 @@ then
 echo "Everything's up to date, good job!"
 else
 echo "Uh-oh! You might want to pull/push"
+fi
+}
 
-
+function check_changes() {
 git diff > changes.log
+}
 
-
+function check_TODO() { 
 tag=(grep -r "#TODO" --exclude="todo.log" --exclude="ProjectAnalyze.sh" --exclude="changes.log")
 $tag > todo.log
+}
 
-
+function check_haskellerrors () {
 find -name "*.hs" | xargs -I {} ghc -fno-code {} &> error.log
+}
