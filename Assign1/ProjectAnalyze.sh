fa655e6f022cd620c636ec6e2999a2494926b7b6
Khizar Mohammad Siddiqui <siddik1@mills.cas.mcmaster.ca>
2018-02-26 21:06:11 -0500

    Mandatory Tasks

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-git status | grep "Your branch is up-to-date"
+
+val= (git status | grep "Your branch is up-to-date")
+
+if [$val == "Your branch is up-to-date"]
+then
+echo "Everything's up to date, good job!"
+else
+echo "Uh-oh! You might want to pull/push"
+
+
+git diff > changes.log
+
+
+tag=(grep -r "#TODO" --exclude="todo.log" --exclude="ProjectAnalyze.sh" --exclude="changes.log")
+$tag > todo.log
+
+
+find -name "*.hs" | xargs -I {} ghc -fno-code {} &> error.log
